a20deb886da0b002cafe7ac5e6b677860d37a545
christopherleavitt99 <cml95@byui.edu>
2023-02-02 09:00:40 -0700

    fixed controllers

--- controllers/listItem.js
+++ controllers/listItem.js
@@ -1,17 +1,45 @@
 const db = require('../models');
 const listItem = db.listItem;
 
+exports.create = (req, res) => {
+  // Validate request
+ 
+
+  const listItem = new listItem(req.body);
+  listItem
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while creating the list.'
+      });
+    });
+};
+
+exports.getAll = (req, res) => {
+  listItem.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while retrieving list.'
+      });
+    });
+};
+
 exports.getlistItem = (req, res) => {
   const listItem = req.params.listItem;
   listItem.find({ listItem: listItem })
     .then((data) => {
-      if (!data) res.status(404).send({ message: 'Not found listItem with name: ' + listItem });
-      else res.send(data[0]);
+      res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving listItem with listItem=' + listItem,
-        error: err
+        message: err.message || 'Some error occurred while retrieving list.'
       });
     });
 };
