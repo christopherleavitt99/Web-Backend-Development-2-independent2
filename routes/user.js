9bacabd35e4f60f02e3b31e4c0caa5ecf42e7dc9
christopherleavitt99 <cml95@byui.edu>
2023-02-03 20:15:06 -0700

    updated user files

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-const userController = require('../controllers/user');
+// const userController = require('../controllers/user');
 
-router.get('/', userController.getAll);
+// router.get('/', userController.getAll);
 
-router.get('/:username', userController.getUser);
+// router.get('/:username', userController.getUser);
 
-router.post('/', userController.create);
+// router.post('/', userController.create);
 
-module.exports = router;
+// module.exports = router;
